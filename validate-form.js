48c6ffde29a6d04904772533f2d5aa4c06c538b6
Adam Brodzinski <adambrodzinski@gmail.com>
2014-06-15 14:58:03 -0400

    add public docs

--- validate-form.js
+++ validate-form.js
@@ -1,20 +1,35 @@
 /*global ValidateForm:true */
 
-
 ValidateForm = {
   _debug: true,
   _validations: [],
 
 
+  // Public: remove any validation visuals on an elements. Removes
+  // the valid and invalid classes.
+  // 
+  // el - The {Object} DOM input node to remove status on
+  //
   clearInputStatus: function(el) {
     $(el).removeClass('is-valid is-invalid');
   },
 
+
+  // Public: run any validations found on `el`s input data tags. Validations
+  // will produce side effects to visually show user there is an error. All
+  // validations will push true or false into validation history array.
+  // 
+  // el - The {Object} DOM input node to validate
+  //
   validateInput: function(el) {
     this.el = el;
     this.$el = $(el);
     this._runValidations();
   },
+
+
+  // private
+
 
   _runValidations: function() {
     log("\n[ValidateForm] running validations on input", this.el);
